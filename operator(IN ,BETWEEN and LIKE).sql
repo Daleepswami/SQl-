5a369a043e1c8bcdf4c776ffeee8efa75258f086
Daleepswami <daleepswami22170@gmail.com>
2025-12-16 00:59:43 +0530

    IN ,BETWEEN AND LIKE OPERATOR ('%' AND '__')

---
+++ operator(IN ,BETWEEN and LIKE).sql
@@ -0,0 +1,147 @@
+use  world;
+SELECT 
+    *
+FROM
+    city;
+
+
+SELECT 
+    name, population
+FROM
+    city;
+describe city;
+
+-- where clause we will filter the data 
+SELECT 
+    *
+FROM
+    city
+WHERE
+    district = 'Zuid-Holland'; 
+    
+SELECT 
+    *
+FROM
+    city
+WHERE
+    countrycode = 'ARG';
+-- -----  6 more than 6 lakh----------------
+SELECT 
+    *
+FROM
+    city
+WHERE
+    population > 600000;
+-- <> != (not equals to )
+SELECT 
+    *
+FROM
+    city
+WHERE
+    countrycode <> 'ARG';
+    
+-- in operator :- In operator  used to filter the data in the specific set of values
+SELECT 
+    *
+FROM
+    country
+WHERE
+    continent = 'North America';
+SELECT 
+    *
+FROM
+    country
+WHERE
+    continent = 'Europe';
+-- getting countries in below continents
+SELECT 
+    *
+FROM
+    country
+WHERE
+    continent IN ('North America' , 'Europe', 'asia');
+    
+SELECT 
+    name, continent, indepyear
+FROM
+    country
+WHERE
+    indepyear IN (1901 , 1960);
+
+-- between operator :- between operator  used to filter the data in the specific give range values
+
+
+    SELECT 
+    name, continent, indepyear
+FROM
+    country
+WHERE
+    indepyear BETWEEN 1901 AND 1960;
+
+    
+-- questions practice --
+-- Q.1 get the code country region where region is not middle east 
+-- Q.2 get the name indepyear and population where 10 % increase in the population
+-- Q.3 get the all the columns of the country where lifeExpectancy from 38.3 to 66.4
+-- Q.4 get the name continent population,GNP  columns from country where population is not from 5 k to 200k
+select * from country;
+SELECT 
+    name, code, Region
+FROM
+    country
+WHERE
+    region != 'Middle East';
+SELECT 
+    name,
+    indepyear,
+    population + population * 0.1 AS newpopulation
+FROM
+    country;
+SELECT 
+    *
+FROM
+    country
+WHERE
+    lifeExpectancy IN (38.3 , 66.4); 
+SELECT 
+    name, continent, population, GNP
+FROM
+    country
+WHERE
+    population NOT BETWEEN 5000 AND 200000;
+
+-- like  operator :- search the pattern in keys 
+-- to meet the pattern
+-- some special character is called wild card character 
+-- (I) "%" [we are finding zero or more character]
+SELECT 
+    name, continent
+FROM
+    country
+WHERE
+    name LIKE 'A%';
+SELECT 
+    name, continent
+FROM
+    country
+WHERE
+    name LIKE '%a';
+SELECT 
+    name, continent
+FROM
+    country
+WHERE
+    name LIKE '%a%';
+-- (II) _(underscore)=> 1 character 
+SELECT 
+    name, continent
+FROM
+    country
+WHERE
+    name LIKE 'I____';
+SELECT 
+    name, continent
+FROM
+    country
+WHERE
+    name LIKE '__a__';
